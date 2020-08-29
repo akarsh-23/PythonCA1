c6ceb29925a052dd79c543cde6cd73fa929b82ed
Akarsh Shrivastava <akarsh_shrivastava@yahoo.com>
2020-08-29 13:33:44 +0530

    Delete start1.py

--- start1.py
+++
@@ -1,8 +0,0 @@
-def distinctNumbers(List):
-    list_set = set(List)
-    List = list(list_set)
-    return List
-
-
-List = [int(x) for x in input("Enter values: ").split()]
-print(distinctNumbers(List))
